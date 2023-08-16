30a84c0b0aacd40221e73f8a7e12d6dd1e92f4ac
MarioGuTe <mariogutierreztello@gmail.com>
2023-08-16 01:58:19 -0400

    add logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
+import { useState, useEffect } from "react";
 import "./index.css";
 
+// Statefull ya que no solo "pinta algo" si no que también contiene lógica JS
+
 function App() {
-  return <div className="App"></div>;
+  const [dependency1, setDependency1] = useState(1);
+  const [dependency2, setDependency2] = useState(1);
+
+  // ciclo de vida: mount(montado), update(actualizaciones), unmount(cundo se visita otra página)
+  useEffect(() => {
+    console.log("only dependency1", dependency1, dependency2);
+  }, [dependency1]); //array of dependencies
+
+  useEffect(() => {
+    console.log("only dependency2", dependency1, dependency2);
+  }, [dependency2]);
+
+  useEffect(() => {
+    console.log("change both", dependency1, dependency2);
+  }, [dependency1, dependency2]);
+
+  return (
+    <div className="App">
+      <button onClick={() => setDependency1((prev) => prev + 1)}>
+        Change dependency1
+      </button>
+      <button onClick={() => setDependency2((prev) => prev + 1)}>
+        Change dependency2
+      </button>
+      <button
+        onClick={() => {
+          setDependency1((prev) => prev + 1);
+          setDependency2((prev) => prev + 1);
+        }}
+      >
+        Change both
+      </button>
+    </div>
+  );
 }
 
 export default App;
